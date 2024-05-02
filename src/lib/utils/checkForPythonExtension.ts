105f12a608ab883b886c89a5c7a2764866a9daa1
baptistelechat <baptistelechat@outlook.fr>
2024-05-02 18:24:07 +0200

    🐛 (Python extension) - Manage the case where the Python extension is disabled

--- src/lib/utils/checkForPythonExtension.ts
+++ src/lib/utils/checkForPythonExtension.ts
@@ -2,28 +2,40 @@ import * as vscode from "vscode";
 
 const checkForPythonExtension = () => {
   // Vérifier si l'extension Python est installée
-  const pythonExtension = vscode.extensions.getExtension("ms-python.python");
+  const pythonExtensionId = "ms-python.python";
+  const pythonExtension = vscode.extensions.getExtension(pythonExtensionId);
+
   if (!pythonExtension) {
     vscode.window
       .showErrorMessage(
-        "pyRevit with VSCode | L'extension Python est requise pour utiliser cette extension. Voulez-vous l'installer maintenant ?",
-        "Installer"
+        "pyRevit with VSCode | L'extension Python est requise pour utiliser cette extension. Voulez-vous l'installer ou l'activer maintenant ?",
+        "Installer",
+        "Activer"
       )
       .then((action) => {
         if (action === "Installer") {
-          vscode.commands.executeCommand("workbench.extensions.view");
           vscode.commands.executeCommand(
             "workbench.extensions.search",
-            "ms-python.python"
+            pythonExtensionId
+          );
+          vscode.commands.executeCommand(
+            "workbench.extensions.installExtension",
+            pythonExtensionId
+          );
+        }
+        if (action === "Activer") {
+          vscode.commands.executeCommand(
+            "workbench.extensions.search",
+            pythonExtensionId
           );
         }
       });
     return;
   }
 
-  // Si l'extension Python est installée, afficher un message d'installation réussie et poursuivre l'activation de votre extension
+  // Si l'extension Python est installée et activée, poursuivre l'activation de votre extension
   vscode.window.showInformationMessage(
-    "pyRevit with VSCode | L'extension Python est installée. Vous pouvez maintenant utiliser cette extension."
+    "pyRevit with VSCode | L'extension Python est installée et activée. Vous pouvez maintenant utiliser cette extension."
   );
 };
 
