8539030f61576ca8eb181859d9e325c6f9edfeb1
baptistelechat <baptistelechat@outlook.fr>
2024-05-22 15:48:16 +0200

    ✨ (ShowQuickPick) - Displays a black or white lucide icon, depending on the VSCode theme, with the icon name

--- src/lib/utils/components/utils/selectLucideIcon.ts
+++ src/lib/utils/components/utils/selectLucideIcon.ts
@@ -12,23 +12,40 @@ const selectLucideIcon = async (): Promise<string | undefined> => {
 
   const files = fs.readdirSync(lucideIconsUri.fsPath);
 
-  const iconNames: string[] = [];
+  const items: vscode.QuickPickItem[] = [];
 
   files.forEach((file) => {
     if (path.extname(file) === ".svg") {
       const iconName = path.basename(file, ".svg");
-      iconNames.push(iconName);
+      const iconPath = path.join(lucideIconsPath, file);
+      const iconSvg = fs.readFileSync(iconPath, "utf8");
+      const whiteIconSvg = iconSvg.replace(
+        'stroke="currentColor"',
+        'stroke="white"'
+      );
+      const buffer = Buffer.from(whiteIconSvg, "utf8");
+      const encodedWhiteIconSvg = buffer.toString("base64");
+      const item: vscode.QuickPickItem = {
+        label: iconName,
+        iconPath: {
+          light: vscode.Uri.file(iconPath),
+          dark: vscode.Uri.parse(
+            `data:image/svg+xml;base64,${encodedWhiteIconSvg}`
+          ),
+        },
+      };
+      items.push(item);
     }
   });
 
   return new Promise((resolve, reject) => {
     vscode.window
-      .showQuickPick(iconNames, {
+      .showQuickPick(items, {
         placeHolder: "🎨 Sélectionnez une icône Lucide",
       })
       .then((selectedIcon) => {
         if (selectedIcon) {
-          const svgFileName = selectedIcon + ".svg";
+          const svgFileName = selectedIcon.label + ".svg";
           const svgPath = path.join(lucideIconsPath, svgFileName);
           resolve(svgPath);
         } else {
